6e44727fb75b5dbde313d5d46c18b8c786f47441
yiyu <yiyu4755@gmail.com>
2023-09-05 17:51:25 +0800

    user-center-front

---
+++ src/constans/index.ts
@@ -0,0 +1,4 @@
+export const SYSTEM_LOGO="https://5b0988e595225.cdn.sohucs.com/images/20200308/9c8f8e282a1441639bf4887183875a16.png"
+
+export const BILI_BILI="https://space.bilibili.com/186774608?spm_id_from=333.1007.0.0"
+
